64edb8d4a32cea8106a3b174e2d2c2744e56eea4
Issa Zaytoun <issa.zaytoun@googlemail.com>
2026-08-04 09:32:12 +0000

    Deploy MOTORHALLE to GitHub Pages

---
+++ _next/static/3Lw-R1lzM-pxtT5b6C-5A/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ShareYourCards//_next/:path+",
+        "destination": "/ShareYourCards/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
